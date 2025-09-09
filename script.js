b11401cfa71c1a2f06a62f588a9fd02eec590636
Fabio Castilho <21134162+FabioCastilho@users.noreply.github.com>
2025-09-08 23:49:48 -0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const filterButtons = document.querySelectorAll(".filtros-area .btn");
+  const projects = document.querySelectorAll(".projeto");
+
+function normalizeText(text) {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+}
+
+  filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const filter = normalizeText(button.textContent);
+
+      projects.forEach(project => {
+        const category = normalizeText(project.getAttribute("data-category"));
+
+        if (filter === "todos" || category === filter) {
+          project.style.display = "flex";
+        } else {
+          project.style.display = "none";
+        }
+      });
+    });
+  });
+});
 
